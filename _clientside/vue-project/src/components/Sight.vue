650f1e760b06ceaa681f8e2ed4e5672b4ca8f123
Franz Stimpfl <franz.stimpfl@agilecoderz.com>
2025-12-01 13:59:12 +0100

    initial commit

---
+++ _clientside/vue-project/src/components/Sight.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class="m-2 bg-gray-200 w-1/4">
+<p>Name: {{sight.name}}</p>
+<p>Adress: {{sight.address}}</p>
+<p>Zip: {{sight.zip}}</p>
+<p>City: {{sight.city}}</p>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    props: ['sight']
+}
+
+
+</script>
